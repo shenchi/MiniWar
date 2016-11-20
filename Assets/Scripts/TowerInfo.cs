17f3c2297a66a3d61b56bb1e87402878f9831f15
Peng Guan <pguan100@gmail.com>
2016-11-19 20:21:23 -0500

    Minor change.

--- Assets/Scripts/TowerInfo.cs
+++ Assets/Scripts/TowerInfo.cs
@@ -8,7 +8,7 @@ public class TowerInfo : NetworkBehaviour
 
     [SyncVar]
     public int health;
-    public Vector2 healthGP;
+    public float healthGP;
 
     public int level;
     public int vision;
