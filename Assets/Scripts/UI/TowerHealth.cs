8fc869aae992e49a77652abb8b4c6f638c42a2d6
Peng Guan <pguan100@gmail.com>
2016-12-03 17:06:54 -0500

    Add health bar for towers.
    Ver. 1

---
+++ Assets/Scripts/UI/TowerHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TowerHealth : MonoBehaviour {
+
+    public TowerInfo thisTower;
+    public Image healthFill;
+
+	// Use this for initialization
+	void Start () {
+        thisTower = GetComponentInParent<TowerInfo>();
+        healthFill = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        healthFill.fillAmount = thisTower.healthGP;
+	}
+}
