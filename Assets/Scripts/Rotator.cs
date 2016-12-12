39e87338588c3dce2a77ee8d0a6b87dd2fdb6527
shenchi <shenchi710@gmail.com>
2016-12-12 18:49:29 -0500

    animation for resource towers

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+
+    public GameObject target;
+
+    public Vector3 axis;
+
+    public float omega;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (null == target)
+            target = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target.transform.localRotation *= Quaternion.AngleAxis(omega * Time.deltaTime, axis);
+    }
+}
---
+++ Assets/Scripts/SnapToCenter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SnapToCenter : MonoBehaviour
+{
+
+    public Renderer target;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (null != target)
+            transform.position = target.bounds.center;
+    }
+}
